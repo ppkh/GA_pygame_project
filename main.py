b1a7e98668a3fa9fbaa8a1c67e124b06a2820da4
ppkh <amirgajfullin8@gmail.com>
2025-01-19 16:12:06 +0500

    enemies

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 import sys
 
 all_sprites = pygame.sprite.Group()
+main_character = pygame.sprite.Group()
+enemy_circles = pygame.sprite.Group()
+point_circles = pygame.sprite.Group()
 circles = pygame.sprite.Group()
 horizontal_borders = pygame.sprite.Group()
 vertical_borders = pygame.sprite.Group()
@@ -18,31 +21,49 @@ class Ball(pygame.sprite.Sprite):
         self.radius = radius
         self.image = pygame.Surface((2 * radius, 2 * radius),
                                     pygame.SRCALPHA, 32)
-        pygame.draw.circle(self.image, pygame.Color(colors[random.randint(0, len(colors) - 1)]),
-                           (radius, radius), radius)
         self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
-        self.vx = random.randint(-5, 5)
-        self.vy = random.randrange(-5, 5)
+
+
+class Enemy(Ball):
+    def __init__(self, radius, x, y):
+        super().__init__(radius, x, y)
+        self.add(enemy_circles)
+        pygame.draw.circle(self.image, pygame.Color('red'),
+                           (radius, radius), radius)
+        self.vx = random.randint(-2, 2)
+        while self.vx == 0:
+            self.vx = random.randint(-2, 2)
+
+        self.vy = random.randrange(-2, 2)
+        while self.vy == 0:
+            self.vy = random.randrange(-2, 2)
 
     def update(self):
-        self.rect = self.rect.move(self.vx, self.vy)
+        #self.rect = self.rect.move(self.vx, self.vy)
+
         if pygame.sprite.spritecollideany(self, horizontal_borders):
             self.vy = -self.vy
             self.rect = self.rect.move(self.vx, self.vy)
         if pygame.sprite.spritecollideany(self, vertical_borders):
             self.vx = -self.vx
             self.rect = self.rect.move(self.vx, self.vy)
-
+        self.rect = self.rect.move(self.vx, self.vy)
         circles.remove(self)
         curcircle = pygame.sprite.Group(self)
-        if pygame.sprite.spritecollideany(self, circles):
-            print(pygame.sprite.groupcollide(curcircle, circles, False, False)[self])
-            for i in pygame.sprite.groupcollide(curcircle, circles, False, False)[self]:
+        if pygame.sprite.spritecollideany(self, enemy_circles):
+            for i in pygame.sprite.groupcollide(curcircle, enemy_circles, False, False)[self]:
                 i.vx, i.vy, self.vx, self.vy = self.vx, self.vy, i.vx, i.vy
-                i.rect = i.rect.move(i.vx, i.vy)
-                self.rect = self.rect.move(self.vx, self.vy)
+                #if not (
+                #        pygame.sprite.spritecollideany(self, horizontal_borders)
+                #        or pygame.sprite.spritecollideany(self, vertical_borders)):
+                #    i.rect = i.rect.move(i.vx, i.vy)
+                #    self.rect = self.rect.move(self.vx, self.vy)
         curcircle.empty()
         circles.add(self)
+        # if pygame.sprite.spritecollideany(self, enemy_circles) and not (
+        #        pygame.sprite.spritecollideany(self, horizontal_borders)
+        #        or pygame.sprite.spritecollideany(self, vertical_borders)):
+        #    self.rect = self.rect.move(self.vx, self.vy)
 
 
 class Border(pygame.sprite.Sprite):
@@ -60,8 +81,8 @@ class Border(pygame.sprite.Sprite):
 
 if __name__ == '__main__':
     pygame.init()
-    width = 500
-    height = 500
+    width = 1240
+    height = 960
     size = width, height
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
@@ -69,11 +90,9 @@ if __name__ == '__main__':
     Border(5, height - 5, width - 5, height - 5)
     Border(5, 5, 5, height - 5)
     Border(width - 5, 5, width - 5, height - 5)
-    Balls = []
-    for i in range(10):
-        Balls.append(Ball(20, random.randint(30, height - 30), random.randint(30, height - 30)))
-    for i in circles:
-        print(i)
+    Enemy_balls = []
+    for i in range(5):
+        Enemy_balls.append(Enemy(20, random.randint(20, width - 20), random.randint(20, height - 20)))
     running = True
     while running:
         screen.fill('white')
@@ -84,5 +103,5 @@ if __name__ == '__main__':
         all_sprites.draw(screen)
         all_sprites.update()
         pygame.display.flip()
-        clock.tick(30)
+        clock.tick(60)
     pygame.quit()
