bfd9f4c6e87be13334fb33b56a85bdb2b3ba96d8
ppkh <amirgajfullin8@gmail.com>
2025-01-21 14:58:18 +0500

    Stars

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ all_sprites = pygame.sprite.Group()
 main_character = pygame.sprite.Group()
 enemy_circles = pygame.sprite.Group()
 point_circles = pygame.sprite.Group()
+stars = pygame.sprite.Group()
 circles = pygame.sprite.Group()
 horizontal_borders = pygame.sprite.Group()
 vertical_borders = pygame.sprite.Group()
@@ -35,9 +36,9 @@ class Enemy(Ball):
         while self.vx == 0:
             self.vx = random.randint(-2, 2)
 
-        self.vy = random.randrange(-2, 2)
+        self.vy = random.randint(-2, 2)
         while self.vy == 0:
-            self.vy = random.randrange(-2, 2)
+            self.vy = random.randint(-2, 2)
 
     def update(self):
         # self.rect = self.rect.move(self.vx, self.vy)
@@ -67,6 +68,15 @@ class Enemy(Ball):
             self.rect.x = width - 50
         if self.rect.y >= height:
             self.rect.y = height - 50
+
+
+class Stars(Ball):
+    def __init__(self, x, y):
+        super().__init__(1, x, y)
+        self.add(stars)
+        pygame.draw.circle(self.image, pygame.Color('white'),
+                           (self.radius, self.radius), self.radius)
+        self.rect = pygame.Rect(random.randint(0, width), random.randint(0, height), 1, 1)
 
 
 class Border(pygame.sprite.Sprite):
@@ -100,11 +110,14 @@ if __name__ == '__main__':
     Border(5, 5, 5, height - 5)
     Border(width - 5, 5, width - 5, height - 5)
     Enemy_balls = []
+    Stars_arr = []
     for i in range(width * height // 72000):
         Enemy_balls.append(Enemy(20, random.randint(50, width - 50), random.randint(50, height - 50)))
+    for i in range(width * height // 10000):
+        Stars(random.randint(50, width - 50), random.randint(50, height - 50))
     running = True
     while running:
-        screen.fill('white')
+        screen.fill('black')
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -115,10 +128,15 @@ if __name__ == '__main__':
                 Enemy_balls.clear()
                 for i in enemy_circles:
                     i.kill()
+                for i in stars:
+                    i.kill()
                 enemy_circles.empty()
+                stars.empty()
                 for i in range(width * height // 72000):
-                    Enemy_balls.append(Enemy(20, random.randint(50, width - 50), random.randint(50, height - 50)))
+                    Enemy_balls.append(Enemy(20, random.randint(0, width), random.randint(0, height)))
 
+                for i in range(width * height // 10000):
+                    Stars(random.randint(0, width), random.randint(0, height))
                 horizontal_borders.empty()
                 vertical_borders.empty()
                 Border(5, 5, width - 5, 5)
@@ -126,8 +144,10 @@ if __name__ == '__main__':
                 Border(5, 5, 5, height - 5)
                 Border(width - 5, 5, width - 5, height - 5)
 
+        # for i in range(width * height // 70000):
+        #    star = pygame.draw.rect(screen, 'white', (random.random() * width, random.random() * height, 1, 1))
         all_sprites.draw(screen)
         all_sprites.update()
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(120)
     pygame.quit()
